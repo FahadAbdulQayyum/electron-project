67e1eaf2c1024eb5f070080bd746843d45f3daa3
Fahad Abdul Qayyum <fahad.ocreateo@gmail.com>
2025-07-10 12:04:03 +0500

    Logo implemented

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'public/favicon.ico'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
